d44b516fce5b9fc8dcf323ae7574aa1dcd6c4ece
polika1122@mail.ru <polika1122@mail.ru>
2023-10-30 18:20:51 +0500

    Task1 complete

---
+++ Tyuiu.AvaevaPD.Sprint4.Task1.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AvaevaPD.Sprint4.Task1.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AvaevaPD.Sprint4.Task1.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a979a68-08c6-4bdc-b5fa-30871801c16d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
